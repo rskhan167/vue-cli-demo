281f4d47f2e05b4938ff42121c37309c15d41d03
rskhan167 <rs.khan167@gmail.com>
2020-07-24 17:15:27 +0530

    final commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import home from './components/home.vue';
 
 Vue.config.productionTip = false
 
+Vue.component('app-servers', home);
+
 new Vue({
+  //el: '#app',
   render: h => h(App),
 }).$mount('#app')
