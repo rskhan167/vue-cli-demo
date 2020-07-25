1b214b5514c28e15576ff51498259e477976cd03
rskhan167 <rs.khan167@gmail.com>
2020-07-25 15:50:03 +0530

    components communication

---
+++ src/components/User.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an Awesome user!</p>
+        <button @click="changeName">Change my name</button>
+        <hr>
+        <div class="row">
+            <div class="userDetail">
+                <app-user-detail :name="name"></app-user-detail>
+            </div>
+            <div class="userEdit">
+                <app-user-edit></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data: function(){
+            return {
+                name: 'Max'
+            };
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        },
+        methods: {
+            changeName(){
+                this.name = 'Anna';
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+    .row {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+    }
+    .userDetail, .userEdit {
+        padding: 15px;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import home from './components/home.vue';
 
 Vue.config.productionTip = false
-
-Vue.component('app-servers', home);
 
 new Vue({
   //el: '#app',
